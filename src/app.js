0e8a97eb360ed22fec22d220cf1f8852794ae91e
Max Strübing <max_struebing@gmx.de>
2016-09-28 15:49:51 +0200

    chore: introduce env variables

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import koa from 'koa';
 
 const app = koa();
+const port = process.env.PORT || 3000;
 
 app.use(function * (next) {
 	this.response.body = 'hello world';
 	yield * next;
 });
 
-app.listen(3000);
+console.log(process.env.NODE_ENV || 'Development');
+
+app.listen(port);
