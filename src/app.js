7a03c63556ae3f7824b4b902f86415e3de97b810
Max Strübing <struebing@sitegeist.de>
2016-09-29 13:21:02 +0200

    task: beautify app output

--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@ import koa from 'koa';
 
 const app = koa();
 const port = process.env.PORT || 3000;
+const context = process.env.NODE_ENV || 'Development';
 
 app.use(function * (next) {
 	this.response.body = 'hello world';
 	yield * next;
 });
 
-console.log(process.env.NODE_ENV || 'Development');
+console.log(`App is running in ${context} context on port ${port}`);
 
 app.listen(port);
